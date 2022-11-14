99ed856d0e4cabc0269c82cf6f2ff9470bdfaddf
mohiandipta <mohianmostofa@gmail.com>
2022-11-14 16:25:27 +0600

    switch case added

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const PORT = process.env.PORT || 5001
 
 const server = http.createServer((req, res) => {
 
+    switch (req.method) {
+        case "GET":
+            getReq(req, res)
+            break;
+        case "POST":
+            postReq(req, res)
+            break;
+        case "PUT":
+            putReq(req, res)
+            break;
+        case "DELETE":
+            deleteReq(req, res)
+            break;
+        default:
+            break;
+    }
+
     res.statusCode = 200
     res.setHeader("content-type", "application/json")
     res.write(JSON.stringify({ message: 'Hello happy coding!' }))
